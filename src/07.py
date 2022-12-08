224815147b22a8a99074625777f8270648ff5b13
sroccaserra <sroccaserra@yahoo.com>
2022-12-08 10:04:25 +0100

    ✨ [day 07] Solve part one

---
+++ src/07.py
@@ -0,0 +1,91 @@
+import fileinput
+from re import split
+from copy import deepcopy
+
+
+def solve_1(logs):
+    commands = []
+    log_stack = deepcopy(logs)
+    log_stack.reverse()
+    limit = 10
+    while log_stack:
+        command = []
+        while True:
+            command.append(log_stack.pop())
+
+            if len(log_stack) == 0 or log_stack[-1].startswith('$'):
+                break;
+        commands.append(command)
+    session = { 'root': None, 'cwd': None }
+    for command in commands:
+        instruction = command[0].split()
+        if instruction[1] == 'cd':
+            cd(session, instruction[2])
+        elif instruction[1] == 'ls':
+            for line in command[1:]:
+                process_ls_output_line(session, line)
+        else:
+            raise 'Unknown Instruction: ' + instruction[1]
+        limit -= 1
+
+    root = session['root']
+    result = 0
+    stack = [root]
+    while stack:
+        cwd = stack.pop()
+        size = cwd['size']
+        if size <= 100000:
+            result += size
+        children = cwd['children']
+        for child_name in children:
+            stack.append(children[child_name])
+
+    return result
+
+
+def cd(session, dirname):
+    if session.get('root') is None:
+        root = create_dir(dirname, None)
+        session['root'] = root
+        session['cwd'] = root
+    elif dirname == '..':
+        session['cwd'] = session['cwd']['parent']
+    else:
+        cwd = session['cwd']
+        next_dir = mkdir_p(session, dirname)
+        session['cwd'] = next_dir
+
+
+def create_dir(dirname, parent):
+    return {'name': dirname, 'parent': parent, 'children': {}, 'size': 0}
+
+
+def mkdir_p(session, dirname):
+    cwd = session['cwd']
+    if dirname in cwd['children']:
+        return cwd['children'][dirname]
+    new_dir = create_dir(dirname, cwd)
+    cwd['children'][dirname] = new_dir
+    return new_dir
+
+
+def process_ls_output_line(session, line):
+    words = line.split()
+    if words[0] == 'dir':
+        mkdir_p(session, words[1])
+    else:
+        cwd = session['cwd']
+        size = int(words[0])
+        cwd['size'] += size
+        update_parents_sizes(cwd, size)
+
+
+def update_parents_sizes(directory, size):
+    cwd = directory
+    while cwd['parent'] is not None:
+        cwd = cwd['parent']
+        cwd['size'] += size
+
+
+logs = [line.rstrip() for line in fileinput.input()]
+print(solve_1(logs))
