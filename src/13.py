f0436c0958a80aae7258d5305c7a13bccb1a7771
sroccaserra <sroccaserra@yahoo.com>
2022-12-13 12:02:18 +0100

    ♻️ Fix comparison function name

--- src/13.py
+++ src/13.py
@@ -9,7 +9,7 @@ def solve_1(packet_pairs):
     result = 0
     for i in range(len(packet_pairs)):
         lhs, rhs = packet_pairs[i]
-        if are_in_order(lhs, rhs) < 0:
+        if compare_packets(lhs, rhs) < 0:
            result += 1+i
     return result
 
@@ -21,13 +21,12 @@ def solve_2(packet_pairs):
         packets.append(rhs)
     packets.append([[2]])
     packets.append([[6]])
-    sorted_packets = sorted(packets, key=cmp_to_key(are_in_order))
+    sorted_packets = sorted(packets, key=cmp_to_key(compare_packets))
     return (sorted_packets.index([[2]])+1)*(sorted_packets.index([[6]])+1)
 
 
-def are_in_order(lhs, rhs):
-    both_ints = isinstance(lhs, int) and isinstance(rhs, int)
-    if both_ints:
+def compare_packets(lhs, rhs):
+    if isinstance(lhs, int) and isinstance(rhs, int):
         if lhs == rhs:
             return NEXT
         return lhs - rhs
@@ -35,7 +34,7 @@ def are_in_order(lhs, rhs):
     rhs = coerce_list(rhs)
     common_len = min(len(lhs), len(rhs))
     for i in range(common_len):
-        cmp = are_in_order(lhs[i], rhs[i])
+        cmp = compare_packets(lhs[i], rhs[i])
         if cmp != NEXT:
             return cmp
     if len(lhs) == len(rhs):
