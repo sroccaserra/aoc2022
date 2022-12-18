9537fcef1da553924bec1d0fb7bfd37c00863bf9
sroccaserra <sroccaserra@yahoo.com>
2022-12-18 02:51:19 +0100

    ✨ [day 17] Solve part two

--- src/17.py
+++ src/17.py
@@ -49,6 +49,52 @@ def solve_1(commands):
     return level
 
 
+def solve_2(commands):
+    target = 1_000_000_000_000
+    command_it = cycle(commands)
+    rock_it = make_rock_iterator()
+    level = 0
+    previous_level = 0
+    cavern = make_cavern()
+    d_levels = []
+    levels = []
+    cycle_maybe = None
+    for _ in range(target):
+        rock = next(rock_it)
+
+        fall(cavern, level, command_it, rock)
+        for (x, y) in cavern.keys():
+            if cavern[(x,y)] == '#':
+                if level < y:
+                    level = y
+        for y in range(level+3+4):
+            cavern[(-1, y)] = '|'
+            cavern[(CAVE_W, y)] = '|'
+
+        levels.append(level)
+        d_level = level - previous_level
+        d_levels.append(d_level)
+        cycle_maybe = find_largest_cycle(d_levels)
+        if cycle_maybe is not None and cycle_maybe[2] > 30:
+            break
+        previous_level = level
+    assert cycle_maybe is not None
+    last_cycle_start = cycle_maybe[0]+1
+    last_cycle_end = cycle_maybe[1]
+    cycle_size = cycle_maybe[2]
+    missing_rocks = target - cycle_maybe[1]
+    missing_cycles = missing_rocks // cycle_size
+    by_cycle = levels[last_cycle_end]-levels[last_cycle_start-1]
+    remainder = missing_rocks % cycle_size
+    target_level = level + missing_cycles*by_cycle
+    d_cycle = d_levels[last_cycle_start:last_cycle_end+1]
+    for i in range(remainder-1):
+        d_level = d_cycle[i]
+        target_level += d_level
+
+    return target_level
+
+
 def make_cavern():
     cavern = {}
     for i in range(7):
@@ -76,7 +122,6 @@ def fall(cavern, level, command_it, rock):
         rock_y = next_y
     for (x, y) in rock[DICT].keys():
         cavern[(x+rock_x, y+rock_y)] = '#'
-    # print_cavern(cavern, level, rock, rock_x, rock_y)
 
 
 def intersects(cavern, rock, rock_x, rock_y):
@@ -119,6 +164,27 @@ def rock_tuple(rock_shape):
     return (rock_shape, w, h, rock_dict)
 
 
+def find_largest_cycle(v):
+    last = len(v) - 1
+    m = last - 1
+    found = None
+    while v[last] != v[m]:
+        m -= 1
+    while m + 1 >= len(v)//2:
+        same = True
+        for i in range(1, last-m):
+            if not v[last-i] == v[m-i]:
+                same = False
+                break
+        if same == True:
+            found = (m, last, last - m)
+        m -= 1
+        while 0 < m and v[last] != v[m]:
+            m -= 1
+    return found
+
+
 line = next(fileinput.input()).strip()
 commands = [{'<': -1, '>': 1}[c] for c in line]
 print(solve_1(commands))
+print(solve_2(commands))
